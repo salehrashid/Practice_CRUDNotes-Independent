ce5be3064964158fa65d4c8254b1e43f80c7530f
saleh rashid <saleh.rashid.b@gmail.com>
2022-09-16 09:09:18 +0700

    (add): view model

--- app/src/main/java/com/app/practicecrud/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/app/practicecrud/viewmodel/NoteViewModel.kt
@@ -1,6 +1,80 @@
 package com.app.practicecrud.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.app.practicecrud.database.Notes
+import com.app.practicecrud.repository.NoteRepository
+import kotlinx.coroutines.launch
 
-class NoteViewModel: ViewModel() {
+class NoteViewModel(private val repository: NoteRepository) : ViewModel() {
+    val inputNote = MutableLiveData<String?>()
+    val saveOrUpdateButtonText = MutableLiveData<String>()
+    val clearAllOrDeleteButtonText = MutableLiveData<String>()
+
+    val notes = repository.notes
+    private lateinit var noteToUpdateOrDelete: Notes
+    private var isUpdateOrDelete = false
+
+    init {
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear All"
+    }
+
+    fun iniUpdateAndDelete(notes: Notes) {
+        inputNote.value = notes.note
+        isUpdateOrDelete = true
+        noteToUpdateOrDelete = notes
+        saveOrUpdateButtonText.value = "Update"
+        clearAllOrDeleteButtonText.value = "Clear"
+    }
+
+    fun saveOrUpdate() {
+        if (isUpdateOrDelete) {
+            noteToUpdateOrDelete.note = inputNote.value!!
+
+        }
+    }
+
+    fun clearAllOrDelete() {
+        if (isUpdateOrDelete) {
+            delete(noteToUpdateOrDelete)
+        } else {
+            clearAll()
+        }
+    }
+
+    private fun insert(notes: Notes) {
+        viewModelScope.launch {
+            repository.insert(notes)
+        }
+    }
+
+    private fun update(notes: Notes) {
+        viewModelScope.launch {
+            repository.update(notes)
+
+            inputNote.value = null
+            isUpdateOrDelete = false
+            saveOrUpdateButtonText.value = "Save"
+            clearAllOrDeleteButtonText.value = "Clear All"
+        }
+    }
+
+    private fun delete(notes: Notes) {
+        viewModelScope.launch {
+            repository.delete(notes)
+
+            inputNote.value = null
+            isUpdateOrDelete = false
+            saveOrUpdateButtonText.value = "Save"
+            clearAllOrDeleteButtonText.value = "Clear All"
+        }
+    }
+
+    private fun clearAll() {
+        viewModelScope.launch {
+            repository.deleteAll()
+        }
+    }
 }
--- app/src/main/java/com/app/practicecrud/viewmodel/NoteViewModelFactory.kt
+++ app/src/main/java/com/app/practicecrud/viewmodel/NoteViewModelFactory.kt
@@ -1,4 +1,16 @@
 package com.app.practicecrud.viewmodel
 
-class NoteViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.app.practicecrud.repository.NoteRepository
+import java.lang.IllegalArgumentException
+
+class NoteViewModelFactory(private val repository: NoteRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NoteViewModel::class.java)){
+            return NoteViewModel(repository) as T
+        }else{
+            throw IllegalArgumentException("Unknown ViewModel Class")
+        }
+    }
 }
